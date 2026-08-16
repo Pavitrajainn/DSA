f1ef25052f80164afd8faae957211b0cdb9b2896
pavitra jain <jainpavitra00@gmail.com>
2026-08-17 01:07:34 +0530

    Time: 53 ms (8.52%), Space: 37.3 MB (6.95%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hash_dup = {}
+        for i in nums:
+            hash_dup[i] = hash_dup.get(i,0)+1
+        for x in hash_dup.values():
+            if x >= 2:
+                return True 
+                break
+        else:
+             return False

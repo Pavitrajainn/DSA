4596c56ec17280eb8af050e6f3b8e9a6e1084b1f
pavitra jain <jainpavitra00@gmail.com>
2026-09-13 10:43:00 +0530

    Time: 49 ms (36.84%), Space: 19 MB (93.43%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -9,7 +9,7 @@ class Solution:
           r = n % 10
           rev = r + rev * 10
           n = n // 10
-      if  rev > (2**31 - 1):
+      if rev < (-(21**31)) or rev > (2**31 - 1):
         return 0
       if x < 0 : 
          return (-rev)

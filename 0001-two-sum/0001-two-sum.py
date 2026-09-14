e547ffe362480a226a96fb122984ab6975fc9d44
pavitra jain <jainpavitra00@gmail.com>
2026-09-14 16:09:22 +0530

    Time: 2323 ms (9.47%), Space: 13.3 MB (37.18%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        arr = []
+        n = len(nums)
+        for i in range(0,n):
+            for j in range(i+1,n):
+                if nums[i] +nums[j] == target:
+                   arr.append(i)
+                   arr.append(j)
+        return(arr)
+      
+      
+        

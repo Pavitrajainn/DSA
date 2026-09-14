05b916d998a4dfb9f1c3e06f89f8207e2916dcdb
pavitra jain <jainpavitra00@gmail.com>
2026-09-14 16:09:48 +0530

    Time: 2316 ms (9.55%), Space: 12.9 MB (82.82%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,7 +4,7 @@ class Solution(object):
         n = len(nums)
         for i in range(0,n):
             for j in range(i+1,n):
-                if nums[i] +nums[j] == target:
+                if nums[i] + nums[j] == target:
                    arr.append(i)
                    arr.append(j)
         return(arr)
